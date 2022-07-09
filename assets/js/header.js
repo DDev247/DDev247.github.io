9f97b1370df5432a38a4eb8a9e610f893a6613be
DDev247 <dawidgaming.ro@gmail.com>
2022-07-09 14:10:21 +0200

    header.js fixed stupid mistake

--- assets/js/header.js
+++ assets/js/header.js
@@ -19,6 +19,7 @@
 */
 
 const header = document.createElement("header");
+header.className = "topbar-ddev";
 header.id = "header";
 
 const link1 = document.createElement("a");
@@ -32,50 +33,51 @@ link1Img.alt = "Home";
 link1.appendChild(link1Img);
 
 const link2 = document.createElement("a");
-link1.className = "topbar-link";
-link1.href = "https://ddev247.github.io/dev";
-link1.text = "Developer";
+link2.className = "topbar-link";
+link2.href = "https://ddev247.github.io/dev";
+link2.text = "Developer";
 
 const link3 = document.createElement("a");
-link1.className = "topbar-link";
-link1.href = "https://ddev247.github.io/dev/blogs";
-link1.text = "Blogs";
+link3.className = "topbar-link";
+link3.href = "https://ddev247.github.io/dev/blogs";
+link3.text = "Blogs";
 
 const link4 = document.createElement("a");
-link1.className = "topbar-link";
-link1.href = "https://ddev247.github.io/about";
-link1.text = "About";
+link4.className = "topbar-link";
+link4.href = "https://ddev247.github.io/about";
+link4.text = "About";
 
 header.appendChild(link1);
 header.appendChild(link2);
 header.appendChild(link3);
 header.appendChild(link4);
 
-
+// MOBILE HEADER
 
 const header_mobile = document.createElement("header");
-header_mobile.tagName = "header-mobile"
+header_mobile.className = "topnav";
+header_mobile.id = "header-mobile";
 
 const mobile_link1 = document.createElement("a");
-link1.href = "https://ddev247.github.io/home";
+mobile_link1.href = "https://ddev247.github.io/home";
 
 const mobile_link1Img = document.createElement("img");
-link1Img.src = "https://ddev247.github.io/assets/logo25.png";
-link1Img.alt = "Home";
+mobile_link1Img.src = "https://ddev247.github.io/assets/logo25.png";
+mobile_link1Img.alt = "Home";
 
 mobile_link1.appendChild(link1Img);
 
 const mobile_link2 = document.createElement("a");
-link1.href = "https://ddev247.github.io/dev";
-link1.text = "Developer";
+mobile_link2.href = "https://ddev247.github.io/dev";
+mobile_link2.text = "Developer";
 
 const mobile_link3 = document.createElement("a");
-link1.href = "https://ddev247.github.io/dev/blogs";
-link1.text = "Blogs";
+mobile_link3.href = "https://ddev247.github.io/dev/blogs";
+mobile_link3.text = "Blogs";
 
 const mobile_link4 = document.createElement("a");
-link1.href = "https://ddev247.github.io/about";
-link1.text = "About";
+mobile_link4.href = "https://ddev247.github.io/about";
+mobile_link4.text = "About";
 
 header_mobile.appendChild(mobile_link1);
 header_mobile.appendChild(mobile_link2);
@@ -83,4 +85,4 @@ header_mobile.appendChild(mobile_link3);
 header_mobile.appendChild(mobile_link4);
 
 document.getElementById("content").insertBefore(header, null);
-document.getElementById("header").after(header, null);
+document.getElementById("header").after(header_mobile);
